657b222eb3274fb40718ecada28a9d10fe6a3569
GT-ZhangAcer <1029550448@qq.com>
2020-07-12 10:56:39 +0800

    test

--- analysis_color.py
+++ analysis_color.py
@@ -8,49 +8,53 @@ import numpy as np
 import cv2 as cv
 from sklearn.cluster import KMeans
 
-CLASS_NUM = 300  # 颜色分类数，越大分类数越高，效果越好 - 模型大小会增加
+BLOCK_SIZE1D = 60  # 1D细分类块大小，越大分类数越高，效果越好 - 模型大小会增加
+MAX_STEP = 10  # 最大步长，越低效果越好
 IMG_BLOCK = 5000  # 图片采样数，越高越好 - 统计速度变慢
 R_IMG_SIZE = (512, 512)  # 采样分辨率，越高越好 - 可能出现爆炸现象
 IMAGE_DIR_PATH = "val2017"
 
 
-def cvt_infer(c_map):
-    l_map = dict()
-    for c in range(CLASS_NUM):
-        tmp_a = list()
-        for label_a in range(255):
-            if c in c_map[label_a]:
-                tmp_a.append(label_a)
-        if len(tmp_a) > 1:
-            label_a = tmp_a[len(tmp_a) // 2]
-        elif len(tmp_a) == 1:
-            label_a = tmp_a[0]
+def analysis1d(signal, step):
+    """
+    颜色细分类
+    :param signal: A或B通道信号
+    :param step: 步长
+    :return: pix -> label 和 label -> pix 以及 label数量
+    """
+    start = 0
+    end = 1
+    label = 0
+    block_list = dict()
+    color_list = dict()
+    while True:
+        if (label == BLOCK_SIZE1D - 2 and start - end <= MAX_STEP) or end == 255:
+            block_list.update(dict([(k, label) for k in range(start, 255)]))
+            color_list[label] = np.max(signal[start:255]).astype("uint8") + start
+            break
+        elif np.sum(signal[start:end]) >= int(step) or start - end >= MAX_STEP:
+            block_list.update(dict([(k, label) for k in range(start, end)]))
+            color_list[label] = np.argmax(signal[start:end]).astype("uint8") + start
+            start = end
+            end += 1
+            label += 1
         else:
-            continue
-        tmp_b = list()
-        for label_b in range(255):
-            if c == c_map[label_a][label_b]:
-                tmp_b.append(label_b)
-        label_b = tmp_b[len(tmp_b) // 2]
-        if len(tmp_b) == 1:
-            label_b = tmp_b[0]
-        l_map[c] = (label_a, label_b)
-    return l_map
+            end += 1
+    return block_list, color_list, label + 1
 
 
-def k_mean(ipt):
-    model = KMeans(n_clusters=CLASS_NUM)
-    model = model.fit(ipt)
-    model_out = model.predict(ipt)
-    k_map = np.zeros((256, 256)).astype("int64")
-    for index_x in range(256):
-        for index_y in range(256):
-            k_map[index_x][index_y] = model_out[index_x * 256 + index_y]
-    return k_map
+def k_mean(signal, class_num):
+    model = KMeans(n_clusters=5)
+    # 将数据代入到聚类模型中
+    model = model.fit(signal)
+    color_map = [[i, j] for i in range(256) for j in range(256)]
+    model.predict(color_map)
+    color_dict = dict([(k,v) for k,v in zip(color_map)])
 
 
 print("开始统计颜色信号...")
-signal_cache = np.zeros([256, 256]).astype("float16")
+signal_cache_a = np.zeros(255)
+signal_cache_b = np.zeros(255)
 img_num = 0
 for file_id, file in enumerate(os.listdir(IMAGE_DIR_PATH)):
     if file_id == IMG_BLOCK:
@@ -59,24 +63,27 @@ for file_id, file in enumerate(os.listdir(IMAGE_DIR_PATH)):
     img = cv.resize(img, R_IMG_SIZE)
     img = cv.cvtColor(img, cv.COLOR_BGR2LAB)
     im_l, im_a, im_b = cv.split(img)
-    a = np.array(im_a)
-    b = np.array(im_b)
-    for pix_x in range(256):
-        for pix_y in range(256):
-            signal_cache[a[pix_x][pix_y], a[pix_x][pix_y]] += 1e-5
-    print(file_id, "--OK!")
-signal_max = np.max(signal_cache)
-signal_min = np.min(signal_cache)
-rate = (signal_max - signal_min) / 256
-signal_cache /= rate
+    a = np.array(im_a).flatten()
+    b = np.array(im_b).flatten()
+    h_a = np.histogram(a, 255, range=(0, 255))[0].astype("float32")
+    signal_cache_a += h_a / 1000000
+    h_b = np.histogram(b, 255, range=(0, 255))[0].astype("float32")
+    signal_cache_b += h_b / 1000000
+    img_num = file_id
+img_num += 1
+signal_cache_a = signal_cache_a / img_num * 1000000
+signal_cache_b = signal_cache_b / img_num * 1000000
 
-signal = [[x, y, signal_cache[x][y]] for x in range(256) for y in range(256)]
-print("正在生成字典...")
+a_step = np.sum(signal_cache_a) / BLOCK_SIZE1D
+b_step = np.sum(signal_cache_b) / BLOCK_SIZE1D
 
-color_map = k_mean(signal)
-label_map = cvt_infer(color_map)
-print("写入硬盘...")
-with open("./Color.dict", "w", encoding="utf-8")as f:
-    f.write(str([color_map.tolist(), label_map]))
-print("已保存至./Color.dict \n分类数为:", CLASS_NUM)
-print("调用方法为:label = map[A值][B值]")
+mode = input("选择生成模式1或2_")
+if mode == 1:
+    a_dict, al_dict, a_num = analysis1d(signal_cache_a, a_step)
+    b_dict, bl_dict, b_num = analysis1d(signal_cache_b, b_step)
+    print("写入硬盘...")
+    with open("./Color.dict", "w", encoding="utf-8")as f:
+        f.write(str([[a_dict, b_dict], [al_dict, bl_dict]]))
+    print("已保存至./Color1D.dict \n分类数为:", a_num * b_num, "\tA:", a_num, "\tB:", b_num)
+else:
+    pass
